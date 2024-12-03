79ab6ab3bc470722af45d75e412d0edc098f1f80
Vithi3182 <savidhya646@gmail.com>
2024-12-03 21:04:26 +0530

    Initial commit

---
+++ Python/Mnk-trainee-Vidhya_S_A/day1_code.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator!")
+city = input("What's the name of the city u grew up?\n")
+pet_name = input("what's ur favourite pet?\n")
+print("Ur band name is " , city +" " + pet_name)
