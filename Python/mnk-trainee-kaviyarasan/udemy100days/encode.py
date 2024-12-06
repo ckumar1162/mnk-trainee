70c36fe6243fb87d42ef6215fee6be7cb9627933
heart less <you@example.com>
2024-12-06 13:26:11 +0530

    modified and Upload work for 6-12-2024

---
+++ Python/mnk-trainee-kaviyarasan/udemy100days/encode.py
@@ -0,0 +1,8 @@
+from itertools import count
+
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+text = input("type your message:\n").lower()
+shift = int(input("enter the shift number:\n")).__int__()
+
+
