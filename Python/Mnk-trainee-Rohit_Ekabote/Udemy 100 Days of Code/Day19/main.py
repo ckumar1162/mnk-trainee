4a9b72135600ccc962baa4dfa9cd06a3767b2c64
rohitekabote <rohit.ekbote731@gmail.com>
2024-12-06 14:09:49 +0530

    Add files via upload

---
+++ Python/Mnk-trainee-Rohit_Ekabote/Udemy 100 Days of Code/Day19/main.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def move_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def move_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+#screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(move_right, "d")
+screen.onkey(move_left, "a")
+screen.onkey(clear, "c")
+screen.exitonclick()
