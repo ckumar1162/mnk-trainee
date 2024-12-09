8364f8a6e6e9a1cc77068c0f7b33e3dc309755b9
22Ushak <2ushakk@gmail.com>
2024-12-09 12:24:09 +0530

    updated

---
+++ Python/Mnk-trainee-usha/Udemy_100days/Day19_HOF/hof.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+def caluclator(other_function,x,y):
+    print(other_function(x,y))
+caluclator(sub,8,8)
